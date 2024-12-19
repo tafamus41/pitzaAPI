42d360795d5df3d67e705b7d994a4377ab337908
asimov <seyranmustafa4@gmail.com>
2024-12-19 18:36:22 +0300

    p

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -4,18 +4,40 @@
 ------------------------------------------------------- */
 
 const Token = require("../models/token");
+const jwt = require('jsonwebtoken')
 
 module.exports = async (req, res, next) => {
-  const auth = req.headers?.authorization || null; // Token ...tokenkey...
-  const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...TokenKey...]
 
-  if (tokenKey && tokenKey[0] == "Token") {
-    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
-      "userId"
-    );
-    console.log(tokenData);
-    if (tokenData) req.user = tokenData.userId;
+  const auth = req.headers?.authorization || null; // Token ...Tokenkey... || Bearer ...AccessToken...
+  const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...TokenKey...] || ['Bearer', '...AccessToken...]
+
+  if (tokenKey) {
+
+    if (tokenKey[0] == "Token") {
+      // SIMPLE TOKEN:
+      const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+        "userId"
+      );
+      // console.log(tokenData);
+      if (tokenData) req.user = tokenData.userId;
+
+    } else if (tokenKey[0] == 'Bearer') {
+      // JWT:
+
+      // JWT Decode & Verify:
+      jwt.verify(tokenKey[1], process.env.ACCESS_KEY, function(error, accessData) {
+        
+        console.log(accessData)
+        if (accessData) req.user = accessData
+
+      })
+
+    }
   }
-  console.log("---------", req.user);
+
+
+
+  // console.log("---------", req.user);
   next();
+
 };
