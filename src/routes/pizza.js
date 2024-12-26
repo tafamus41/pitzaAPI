5f7ca6cd413248c872d0b4d5d02c1f6685805a6c
asimov <seyranmustafa4@gmail.com>
2024-12-26 03:20:59 +0300

    p

--- src/routes/pizza.js
+++ src/routes/pizza.js
@@ -8,16 +8,43 @@ const router = require('express').Router()
 
 const pizza = require('../controllers/pizza')
 
+/* ------------------------------------------------------- */
+//* UPLOADING * MULTER
+// $ npm i multer
+// https://expressjs.com/en/resources/middleware/multer.html
+
+const multer = require('multer')
+
+// upload middleware:
+const upload = multer({
+    // dest: './upload',
+    storage: multer.diskStorage({
+        destination: './upload',
+        filename: function(req, file, returnCallback) {
+            // console.log(file)
+            // returnCallback(error, fileName)
+            // returnCallback(null, file.originalname)
+            returnCallback(null, Date.now() + '_' + file.originalname)
+        }
+    })
+})
+
+
+/* ------------------------------------------------------- */
+
 // URL: /pizzas
 
 router.route('/')
     .get(pizza.list)
-    .post(pizza.create)
+    // .post(pizza.create)
+    // .post(upload.single('image'), pizza.create) // Tek dosya izin ver
+    .post(upload.array('image'), pizza.create) // Çok dosya izin ver. Tavsiye.
+    // .post(upload.any(), pizza.create) // Çok dosya izin ver, fieldname önemsiz.
 
 router.route('/:id')
     .get(pizza.read)
-    .put(pizza.update)
-    .patch(pizza.update)
+    .put(upload.array('image'), pizza.update)
+    .patch(upload.array('image'), pizza.update)
     .delete(pizza.delete)
 
 /* ------------------------------------------------------- */
